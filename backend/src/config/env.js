2d52aa421f4d8cbd81762210e08c7e6778bc95e9
AmanCrafts <theamanmalikarts@gmail.com>
2026-06-27 16:19:06 +0530

    chore: Initialised the Backend with node express and supabase

---
+++ backend/src/config/env.js
@@ -0,0 +1,5 @@
+import "dotenv/config"
+export const SUPABASE_URL = process.env.SUPABASE_URL
+export const SUPABASE_PUBLISHABLE_KEY = process.env.SUPABASE_PUBLISHABLE_KEY
+export const SUPABASE_SECRET_KEY = process.env.SUPABASE_SECRET_KEY
+export const SUPABASE_JWKS_URL = process.env.SUPABASE_JWKS_URL
